556c78b2e362793d766b3e315d93ce50880fc49b
Rohin Shah <rohinmshah@gmail.com>
2016-05-08 23:47:04 -0700

    Added example that we want to work

--- full-example/simplified-lda-racket.rkt
+++ full-example/simplified-lda-racket.rkt
@@ -1,37 +1,43 @@
 #lang racket
 
-(provide (all-defined-out))
-
-;(require "simplified-lda-rosette.rkt")
-;(require "simplified-lda-lsl.rkt")
-
 (require "rosette-namespace.rkt")
 (require "lsl-namespace.rkt")
-(require "lsl-transformer.rkt")
 
-(define x 4)
-(define y 6)
+;; Goal is for the following program to work:
+(define (assert-positive x) (assert (> x 0)))
 
-(define z
-  (eval `(begin
-           (define (get-middle a b)
-             (define-symbolic x integer?)
-             (define model
-               (synthesize #:forall '()
-                           #:guarantee (begin (assert (> x a))
-                                              (assert (< x b)))))
-             (evaluate x model))
-           (get-middle ,x ,y))
-        rosette-ns))
+(define-constant NUM_WORDS integer?)
+(define-constant NUM_TOPICS integer?)
+(define-constant NUM_DOCUMENTS integer?)
+(define-constant VOCABULARY_SIZE integer?)
 
-(print z)
-(newline)
+;; (define-base-type word (enum-range NUM_WORDS))
+;; (define-base-type topic (enum-range NUM_TOPICS))
+;; (define-base-type document (enum-range NUM_DOCUMENTS))
 
-;(add3 x y z)
+;; TODO: Need to specify types better -- specifically, how do we
+;; specify the range of this relation?
+(define-structure topics (Vector NUM_WORDS topic)
+  (mutable))
 
-;(define fn (padd3 #'x y z))
+(define-structure num1 (Vector NUM_TOPICS integer?)
+  (incremental
+   (lambda (t)
+     (size-of-set ((word w) | (equal? (vector-ref topics w) t))))))
 
-;(define (qadd3 x y z)
-;  (+ x (+ y z)))
+(define-structure num2 (Vector NUM_DOCUMENTS integer?)
+  (incremental
+   (lambda (d)
+     (size-of-set ((topic t) | (exists (word w)
+                                       (and (equal? (vector-ref topics w) t)
+                                            (equal? (document w) d))))))))
 
-(define fn2 (pe (thunk (transform (+ #'x (+ y z))))))
+(define-structure value (integer?)
+  (incremental
+   (lambda ()
+     (sum (/ (* beta num1) (+ (* beta VOCABULARY_SIZE) (sum num2)))))))
+
+(define (go)
+  (for/sum ([i 100])
+    (update-topics! (random NUM_WORDS) (random NUM_TOPICS))
+    value))
