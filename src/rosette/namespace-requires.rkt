1962afc77114bdda60105075c5541c80b08dcb56
Rohin Shah <rohinmshah@gmail.com>
2017-03-05 12:16:17 -0800

    Remove metasketches for now to remove dependency on Synapse

--- src/rosette/namespace-requires.rkt
+++ src/rosette/namespace-requires.rkt
@@ -3,14 +3,15 @@
 (require
  (only-in "grammar/lifted-operators.rkt" operator-info)
  rosette/lib/synthax
- "grammar/grammar.rkt" "grammar/metasketch.rkt" "grammar/sketch.rkt"
+ ;; Removed metasketch for now to remove dependency on Synapse
+ "grammar/grammar.rkt" #;"grammar/metasketch.rkt" "grammar/sketch.rkt"
  "enum-set.rkt" "graph.rkt" "operators.rkt" "types.rkt" "util.rkt")
 
 (provide
  operator-info
  (all-from-out
   rosette/lib/synthax
-  "grammar/grammar.rkt" "grammar/metasketch.rkt" "grammar/sketch.rkt"
+  "grammar/grammar.rkt" #;"grammar/metasketch.rkt" "grammar/sketch.rkt"
   "enum-set.rkt" "graph.rkt" "operators.rkt" "types.rkt" "util.rkt"))
 
 
