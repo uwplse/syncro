0964ca3c9b167af03b2c2c5a09dfc0a4cf43b83c
Sumith1896 <sumith1896@gmail.com>
2017-06-02 16:14:23 -0700

    Make a complete Itzhaky benchmark

---
+++ example/itzhaky/itzhaky-main.rkt
@@ -0,0 +1,42 @@
+#lang incremental
+
+(define SET_SIZE #:type (Integer-type) #:configs [5 3] #:for-types)
+(define-enum-type SET_TYPE SET_SIZE)
+
+(define-structure T? #:type (Set-type SET_TYPE)
+  #:initialize (enum-make-set SET_SIZE)
+  #:deltas
+  [(define (add-elemT! [m SET_TYPE])
+     (enum-set-add! T? m))
+   (define (remove-elemT! [m SET_TYPE])
+     (enum-set-remove! T? m))])
+
+(define-structure S? #:type (Set-type SET_TYPE)
+  #:initialize (enum-make-set SET_SIZE)
+  #:deltas
+  [(define (add-elemS! [m SET_TYPE])
+     (enum-set-add! S? m))
+   (define (remove-elemS! [m SET_TYPE])
+     (enum-set-remove! S? m))])
+
+(define-structure C? #:type (Set-type SET_TYPE)
+  #:value (enum-set-union T? S?)
+  #:depends (T? S?))
+
+(define-structure num-T #:type (Integer-type)
+  #:value (my-for/sum ([x SET_SIZE])
+            (if (enum-set-contains? T? x) 1 0))
+  #:depends (T?))
+
+(define-structure num-S #:type (Integer-type)
+  #:value (my-for/sum ([x SET_SIZE])
+            (if (enum-set-contains? S? x) 1 0))
+  #:depends (S?))
+
+(define-structure num-C #:type (Integer-type)
+  #:value (my-for/sum ([x SET_SIZE])
+            (if (enum-set-contains? C? x) 1 0))
+  #:depends (C?))
+
+(algorithm
+ (void))
