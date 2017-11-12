4496e796a44f6bcdcc355c5c46e08dc70f182b4b
Sumith1896 <sumith1896@gmail.com>
2017-11-13 02:16:09 +0530

    Fix skosette bonsai

--- experiments/bonsai/skosette-bonsai.rkt
+++ experiments/bonsai/skosette-bonsai.rkt
@@ -8,13 +8,12 @@
 
 ; The syntax of our Syncro language, in BNF
 (define syncro-stx
-  '([stmt (((vector-set! . vecb-term) . int-term) . bool-term)
+  '([stmt 
+          (((vector-set! . vecb-term) . int-term) . bool-term)
           (((vector-set! . vecl-term) . int-term) . list-term)
           void-stmt
           (((if . bool-term) . stmt) . stmt)
     ]
-    [vecl-term r1]
-    [vecb-term ψ]
     [listb-term 
         ((vector-ref . vecl-term) . int-term)
         null
@@ -22,15 +21,19 @@
     ]
     [int-term
         one
-        i
+        int-names
     ]
     [bool-term 
         true
         false
-        formula
+        bool-names
         ((list-ref . listb-term) . int-term)
         ((vector-ref . vecb-term) . int-term)
     ]
+    [vecl-term r1]
+    [vecb-term ψ]
+    [int-names i]
+    [bool-names formula]
     [type int           ; integer
           vecb          ; vector of bools
           vecl          ; vector of lists
@@ -95,18 +98,18 @@
         (λ () LISTB)
 
         '((cons . _) . _)
-        (λ (b list)
-            (define list+ (type-expr list env))
+        (λ (b lst)
+            (define lst+ (type-expr lst env))
             (define b+ (type-expr b env))
             (assert (is-instance? b+ BOOL) "cons on a non-bool!")
-            (assert (is-instance? list+ LISTB) "second arg to cons not list type!")
+            (assert (is-instance? lst+ LISTB) "second arg to cons not list type!")
             LISTB)
        
         '((list-ref . _) . _)
-        (λ (list x)
-            (define list+ (type-expr list env))
+        (λ (lst x)
+            (define lst+ (type-expr lst env))
             (define x+ (type-expr x env))
-            (assert (is-instance? list+ LISTB) "list-ref first arg non-list!")
+            (assert (is-instance? lst+ LISTB) "list-ref first arg non-list!")
             (assert (is-instance? x+ INT) "list-ref second arg non-integer!")
             BOOL)
 
@@ -128,7 +131,7 @@
             (assert (if (is-instance? vec+ VECL) (is-instance? y+ LISTB) (is-instance? y+ BOOL)) "third arg to vector-set! is wrong type!")
             INT)
        
-        '((if . _) _ . _)
+        '(((if . _) . _ ) . _)
         (λ (c t f)
             (define c+ (type-expr c env))
             (define t+ (type-expr t env))
@@ -155,27 +158,25 @@
         (λ () 1)
 
         'true
-        (λ () true)
+        (λ () #t)
 
         'false
-        (λ () false)
+        (λ () #f)
 
         'null
-        (λ () null)
+        (λ () '())
 
         '((cons . _) . _)
         (λ (b list)
             (define list+ (eval-expr list env))
             (define b+ (eval-expr b env))
-            (cons b list))
-       
+            (cons b+ list+))
+
         '((list-ref . _) . _)
         (λ (list x)
             (define list+ (type-expr list env))
             (define x+ (type-expr x env))
-            (assert (is-instance? list+ LISTB) "list-ref first arg non-list!")
-            (assert (is-instance? x+ INT) "list-ref second arg non-integer!")
-            BOOL)
+            (list-ref list+ x+))
 
         '((vector-ref . _) . _)
         (λ (vec x)
@@ -191,26 +192,22 @@
             (vector-set! vec+ x+ y+)
             0)
 
-        '((if . _) _ . _)
+        '(((if . _) . _ ) . _)
         (λ (c t f)
             (define c+ (eval-expr c env))
-            (define t+ (eval-expr t env))
-            (define f+ (eval-expr f env))
             (assert (boolean? c+))
-            (if c+ t+ f+))
+            (if c+ (eval-expr t env) (eval-expr f env)))
 
         '_
         (λ (name)
             (define r (table-find* env name))
-            (assert r "Undefined variable usage!")
             r)
 
         ))
 
 (define test-expr
   (expression->enumtree
-    '(((if . formula) .  
-        (((vector-set! . ψ) . i) . false)) . void-stmt)
+    '(((if . formula) . (((vector-set! . ψ) . i) . false)) . void-stmt)
     ))
 
 (define (depth t)
@@ -232,9 +229,9 @@
 (set! init-type-env (table-add init-type-env (symbol->enum 'ψ) VECB))
 (set! init-type-env (table-add init-type-env (symbol->enum 'i) INT))
 (set! init-type-env (table-add init-type-env (symbol->enum 'formula) BOOL))
-; ; (displayln init-type-env)
-; (echo (type-expr test-expr init-type-env))
-
+; (displayln init-type-env)
+(echo (type-expr test-expr init-type-env))
+    
 (displayln "eval test...")
 (define init-eval-env '())
 ; ; (set! init-eval-env (table-add init-eval-env (symbol->enum 'i) 0))
@@ -246,30 +243,33 @@
 
 (newline)
 
-; (displayln "Generating program from IO examples ...")
+(displayln "Generating program from IO examples ...")
 
 (displayln "Time for constructing tree for statement 1 ...")
 (define stmt1* (time (make! syncro-stx 'stmt 6)))
+; (define stmt1* (time test-expr))
 (displayln "Time for typechecking statement 1 ...")
 (define typecheck1 (time (type-expr stmt1* init-type-env)))
 
 (displayln "Time for synthesis by examples")
-
 (define input-output-examples
-  (list (list (vector '(false false) '(true)) 1 true '(true false) '(true false))
-        (list (vector '(false true true) '()) 0 false '(false true) '(false true))
-        (list (vector '(true true false) '(true)) 1 true '(false false) '(false false))))
+  (list (list (vector '(#f #f) '(#t)) 0 #t (vector #t #f) (vector #f #f))
+        (list (vector '(#f #t #t) '(#f)) 1 #t (vector #f #t) (vector #f #f))
+        (list (vector '(#t #t #f) '(#t)) 1 #t (vector #f #f) (vector #f #f))
+        (list (vector '(#f #f #f) '(#f)) 0 #f (vector #t #t) (vector #t #t))
+        (list (vector '(#f #f #f) '(#f)) 1 #f (vector #t #t) (vector #t #t))))
 
 (define synth
    (time (solve (for ([parameters input-output-examples])
-            (match-define (list r1 i formula old-ψ new-ψ)
+            (match-define (list r1 i form old-ψ new-ψ)
                 parameters)
             (set! init-eval-env '())
             (set! init-eval-env (table-add init-eval-env (symbol->enum 'ψ) old-ψ))
             (set! init-eval-env (table-add init-eval-env (symbol->enum 'r1) r1))
             (set! init-eval-env (table-add init-eval-env (symbol->enum 'i) i))
-            (set! init-eval-env (table-add init-eval-env (symbol->enum 'formula) formula))
-            (assert (equal? (eval-expr stmt1* init-eval-env) new-ψ))))))
+            (set! init-eval-env (table-add init-eval-env (symbol->enum 'formula) form))
+            (eval-expr stmt1* init-eval-env)
+            (assert (equal? (table-find* init-eval-env (symbol->enum 'ψ)) new-ψ) "IO assert failed")))))
 
 (if (sat? synth)
    (begin 
